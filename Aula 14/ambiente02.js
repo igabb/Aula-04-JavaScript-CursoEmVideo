d0ad48271c912ed688a9e75bcc2fd482b628c843
iGabb <gb0104@hotmail.com>
2022-03-12 22:14:03 -0300

    Add Aula 14 JS CursoEmVideo

---
+++ Aula 14/ambiente02.js
@@ -0,0 +1,12 @@
+/*
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 2)
+*/
+console.log('Vai começar...')
+for(var c = 1; c <= 6; c++) {
+    console.log(c)
+}
+console.log('Fim!')
